461620cd2e1227ba95edb0d09111172f214b3abd
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-04 14:10:25 +0200

    Intestazione Inserita e informazioni generiche iniziate

--- Bookstore-csharp/Program.cs
+++ Bookstore-csharp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//esercizio stampa info libro.
+
+//titolo
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("--- IL LIBRO DI OGGI: <<Clean Code>> di <<Robert C. Martin Series>> ---");
+
+//informazioni generali
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Informazioni generiche");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("ISBN : <<9780132350884>>");
