b5a34363c12d58ed7d6ea40cc22eb7e94931d06e
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-04 14:27:51 +0200

    Stampa informazioni generiche in console

--- Bookstore-csharp/Program.cs
+++ Bookstore-csharp/Program.cs
@@ -3,6 +3,8 @@
 //titolo
 
 //Dichiaro nelle variabili i valori degli elementi che mi servono
+{ 
+
 string titoloLibro = "Clean Code";
 string nomeAutore = "Robert C. Martin";
 long isbnLibro = 9780132350884;
@@ -11,4 +13,20 @@ double pesoLibro = 0.66;
 double larghezzaLibro = 17.91;
 double lunghezzaLibro = 2.54;
 double profonditàLibro = 23.5;
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+//Corpo Da stampare
+Console.WriteLine("--- IL LIBRO DI OGGI: " + titoloLibro + " di " + nomeAutore + " ---");
+
+//informazioni generali
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Informazioni generiche");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("ISBN: " + isbnLibro);
+Console.WriteLine("Numero delle pagine: " + numeroPagineLibro);
+Console.WriteLine("Peso Libro: " + pesoLibro + " Kg");
+Console.WriteLine("Dimensioni del libro: " + larghezzaLibro + " cm x " + lunghezzaLibro + " cm x " + profonditàLibro + " cm");
+
+}
 
